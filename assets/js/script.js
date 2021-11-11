db5856563e9eaae1c8a31012116da1f314726799
Joona Lusa <joona.lusa@hotmail.com>
2021-11-11 17:06:35 -0500

    questions are being loaded one after the other

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,96 @@
+const numOfAnswers = 4;
+
 var timeLeft = 75;
 var startQuiz = document.querySelector(".start-quiz");
 var timer = null;
+var index = 0;
 
 
 // Array of objects that contain questions, answers, and correct answer
 let quizQuestions = [{
+        "question": "Which company developed JavaScript?",
+        "possibleAnswers": ["Bell Labs", "IBM", "Apple", "Netscape"],
+        "correctAnswer": ""
+    },
+    {
         "question": "In JavaScript, we assign values to variables by which operator?",
         "possibleAnswers": ["===", "+", "=", "=="],
         "correctAnswer": "="
     },
     {
-        "question": "",
-        "possibleAnswers": [],
-        "correctAnswer": ""
+        "question": "Which is NOT a JavaScript Data Type?",
+        "possibleAnswers": ["Boolean", "Number", "Float", "Object"],
+        "correctAnswer": "Float"
     },
     {
-        "question": "",
-        "possibleAnswers": [],
-        "correctAnswer": ""
+        "question": "Which is used for comments in Javascript?",
+        "possibleAnswers": ["//", "<!----!>", "****", "Comments"],
+        "correctAnswer": "//"
     },
     {
-        "question": "",
-        "possibleAnswers": [],
-        "correctAnswer": ""
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "possibleAnswers": ["<head>", "<script>", "<style>", "<body>"],
+        "correctAnswer": "<script>"
     },
     {
-        "question": "",
-        "possibleAnswers": [],
-        "correctAnswer": ""
+        "question": "Arrays in JavaScript can be used to store?",
+        "possibleAnswers": ["Objects", "Numbers", "Strings", "All of the Above"],
+        "correctAnswer": "<script>"
     }
 ]
 
 var timerHandler = function() {
     timeLeft--;
+    // If time is 0 or no more questions, the game ends and the user is notified
     if (timeLeft === 0) {
+        // Stopping the timer
         clearInterval(timer);
-        alert("Game Over!");
+        return;
     }
     document.getElementById("countdown").innerHTML = timeLeft;
 };
 
-var questionHandler = function() {
-    var newSect = document.createElement("div");
-    newSect.className = "center";
-    document.body.appendChild(newSect);
+// Function that handles the question and answer output on browser
+var questionHandler = function(index) {
+    var newForm = document.createElement("form");
+    newForm.className = "center";
+    document.body.appendChild(newForm);
     var newQuest = document.createElement("h1");
-    newQuest.textContent = quizQuestions[0].question;
-    newSect.appendChild(newQuest);
+    newQuest.setAttribute("text-align", "left");
+    newQuest.textContent = quizQuestions[index].question;
+    newForm.appendChild(newQuest);
+
+    for (var count = 0; count < numOfAnswers; count++) {
+        var possibleAnswer = document.createElement("button");
+        possibleAnswer.setAttribute("id", "btn");
+        possibleAnswer.textContent = (count + 1 + ". " + quizQuestions[index].possibleAnswers[count]);
+        newForm.appendChild(possibleAnswer);
+    }
+    // Incrementing the index for the next question
+    index++;
+
+    // Selecting all buttons with id btn and attach them to a variable
+    var nextQuestion = document.querySelectorAll('button[id=btn]');
+
+    // Attach an event listener to each of the buttons
+    nextQuestion.forEach(btn => {
+        btn.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            // Removing previous question from screen
+            newForm.remove();
+
+            // If time is 0 or no more questions, the game ends and the user is notified
+            if (quizQuestions[index] == null) {
+                // Stopping the timer
+                clearInterval(timer);
+                return;
+            }
+
+            // Call next question
+            questionHandler(index);
+        })
+    })
 
 };
 
@@ -55,6 +99,6 @@ var questionHandler = function() {
 // the first question is shown to user and the timer starts counting down
 startQuiz.addEventListener("click", function() {
     document.querySelector("main").style.display = "none";
-    questionHandler();
     timer = setInterval(timerHandler, 1000);
+    questionHandler(index);
 });
