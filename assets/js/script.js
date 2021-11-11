d936d752967a8d0f0da30dd616101b21b4c3a537
Joona Lusa <joona.lusa@hotmail.com>
2021-11-11 18:24:26 -0500

    added more questions and user is notified of their choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
 const numOfAnswers = 4;
+const penalty = 10;
+const correctAnswer = "Correct!";
+const wrongAnswer = "Wrong!";
 
 var timeLeft = 75;
 var startQuiz = document.querySelector(".start-quiz");
 var timer = null;
 var index = 0;
+var userAnswer = "";
 
 
 // Array of objects that contain questions, answers, and correct answer
 let quizQuestions = [{
-        "question": "Which company developed JavaScript?",
-        "possibleAnswers": ["Bell Labs", "IBM", "Apple", "Netscape"],
-        "correctAnswer": ""
+        "question": "String values must be enclosed within what?",
+        "possibleAnswers": ["Curly Brackets", "Double Quotations", "Asterisks", "Commas"],
+        "correctAnswer": "Double Quotations"
     },
     {
         "question": "In JavaScript, we assign values to variables by which operator?",
@@ -18,7 +22,7 @@ let quizQuestions = [{
         "correctAnswer": "="
     },
     {
-        "question": "Which is NOT a JavaScript Data Type?",
+        "question": "Which of the following is NOT a JavaScript Data Type?",
         "possibleAnswers": ["Boolean", "Number", "Float", "Object"],
         "correctAnswer": "Float"
     },
@@ -35,28 +39,42 @@ let quizQuestions = [{
     {
         "question": "Arrays in JavaScript can be used to store?",
         "possibleAnswers": ["Objects", "Numbers", "Strings", "All of the Above"],
-        "correctAnswer": "<script>"
+        "correctAnswer": "All of the Above"
+    },
+    {
+        "question": "Which of the following is not a valid JavaScript variable name?",
+        "possibleAnswers": ["Num", "userStatus", "Break", "myString"],
+        "correctAnswer": "Break"
+    },
+    {
+        "question": "Which method returns the character at the specified index?",
+        "possibleAnswers": ["characterAt()", "getCharAt()", "charAt()", "None of the above"],
+        "correctAnswer": "charAt()"
+    },
+    {
+        "question": "What are the types of Pop up boxes available in JavaScript? ",
+        "possibleAnswers": ["Alert", "Prompt", "Confirm", "All of the above"],
+        "correctAnswer": "All of the above"
     }
 ]
 
 var timerHandler = function() {
     timeLeft--;
-    // If time is 0 or no more questions, the game ends and the user is notified
+    document.getElementById("countdown").innerHTML = timeLeft;
+    // If time is 0 the game ends
     if (timeLeft === 0) {
         // Stopping the timer
         clearInterval(timer);
         return;
     }
-    document.getElementById("countdown").innerHTML = timeLeft;
 };
 
 // Function that handles the question and answer output on browser
-var questionHandler = function(index) {
+var questionHandler = function(index, userAnswer) {
     var newForm = document.createElement("form");
     newForm.className = "center";
     document.body.appendChild(newForm);
     var newQuest = document.createElement("h1");
-    newQuest.setAttribute("text-align", "left");
     newQuest.textContent = quizQuestions[index].question;
     newForm.appendChild(newQuest);
 
@@ -66,6 +84,16 @@ var questionHandler = function(index) {
         possibleAnswer.textContent = (count + 1 + ". " + quizQuestions[index].possibleAnswers[count]);
         newForm.appendChild(possibleAnswer);
     }
+
+    // Creating a breaker line that separates possible answers from the userAnswer
+    var breaker = document.createElement("hr");
+    newForm.appendChild(breaker);
+
+    // Creating an element that captures the result of the answered question
+    var status = document.createElement("h2");
+    status.textContent = userAnswer
+    newForm.appendChild(status);
+
     // Incrementing the index for the next question
     index++;
 
@@ -80,6 +108,19 @@ var questionHandler = function(index) {
             // Removing previous question from screen
             newForm.remove();
 
+            // Checking user answer
+            if (btn.textContent.slice(3) === quizQuestions[index - 1].correctAnswer) {
+                userAnswer = correctAnswer;
+
+            } else {
+                // Penalizing user when answering a question wrong
+                timeLeft -= penalty;
+                userAnswer = wrongAnswer;
+                // Updating the time displayed in browser
+                document.getElementById("countdown").innerHTML = timeLeft;
+
+            }
+
             // If time is 0 or no more questions, the game ends and the user is notified
             if (quizQuestions[index] == null) {
                 // Stopping the timer
@@ -88,7 +129,7 @@ var questionHandler = function(index) {
             }
 
             // Call next question
-            questionHandler(index);
+            questionHandler(index, userAnswer);
         })
     })
 
@@ -100,5 +141,5 @@ var questionHandler = function(index) {
 startQuiz.addEventListener("click", function() {
     document.querySelector("main").style.display = "none";
     timer = setInterval(timerHandler, 1000);
-    questionHandler(index);
+    questionHandler(index, userAnswer);
 });
